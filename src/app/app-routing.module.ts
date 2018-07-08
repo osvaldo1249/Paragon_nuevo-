0ccd97d36206429826e7407d03444c8531a336ba
osvaldo <yair140193@gmail.com>
2018-07-08 15:14:20 -0500

    Incio de paragon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { TerminosComponent } from './terminos/terminos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	  {
+    	path: 'users',
+    	component: UsersComponent
+  	},
+
+  	{
+    	path: 'products',
+    	component: ProductsDetailComponent
+  	},
+
+    {
+      path: 'acerca',
+      component: NosotrosComponent
+    },
+    
+    {
+      path: 'terminos',
+      component: TerminosComponent
+
+    },
+
+  	{
+    	path: '**',
+    	component: HomepageComponent
+  	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductBoxComponent } from './product-box/product-box.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { TerminosComponent } from './terminos/terminos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductBoxComponent,
+    HomepageComponent,
+    ProductsDetailComponent,
+    NosotrosComponent,
+    TerminosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,    
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
